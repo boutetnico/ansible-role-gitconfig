e5e329512e6b583221de23e3d91161e80a96e290
Nicolas Boutet <amd3002@gmail.com>
2023-04-27 18:22:11 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,14 +5,18 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('file,section,line', [
-  ('/root/.gitconfig', 'alias', 'ci = commit'),
-  ('/etc/gitconfig', 'alias', 'remotev = remote -v'),
-  ('/role_test/.git/config', 'user', 'email = root@root'),
-])
+@pytest.mark.parametrize(
+    "file,section,line",
+    [
+        ("/root/.gitconfig", "alias", "ci = commit"),
+        ("/etc/gitconfig", "alias", "remotev = remote -v"),
+        ("/role_test/.git/config", "user", "email = root@root"),
+    ],
+)
 def test_git_config_file(host, file, section, line):
     git_config = host.file(file)
     assert git_config.exists
