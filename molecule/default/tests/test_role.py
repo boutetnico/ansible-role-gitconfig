caaed66480aa168a17ea7b9fc5029552b9c94e79
Nicolas Boutet <amd3002@gmail.com>
2026-02-05 09:09:47 +0100

    Improve test coverage

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -9,9 +9,24 @@ import pytest
         ("/role_test/.git/config", "user", "email = root@root"),
     ],
 )
-def test_git_config_file(host, file, section, line):
+def test_git_config_files_exist(host, file, section, line):
     git_config = host.file(file)
     assert git_config.exists
     assert git_config.is_file
     assert git_config.contains(section)
     assert git_config.contains(line)
+
+
+def test_git_command_works(host):
+    cmd = host.run("git --version")
+    assert cmd.rc == 0
+
+
+def test_system_gitconfig_has_alias_section(host):
+    config = host.file("/etc/gitconfig")
+    assert config.contains("[alias]")
+
+
+def test_user_gitconfig_has_alias_section(host):
+    config = host.file("/root/.gitconfig")
+    assert config.contains("[alias]")
